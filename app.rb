cc63cd67c70813b9808da4d8ca9a385a7a77a28a
Edvin-Lind <edvin.lind@elev.ga.ntig.se>
2024-11-11 14:46:37 +0100

    1:a

--- app.rb
+++ app.rb
@@ -4,4 +4,19 @@ class App < Sinatra::Base
         erb(:"index")
     end
 
+    get '/todo' do
+        @fruits = db.execute('SELECT * FROM todos')
+        erb(:"todos/index")
+    end
+
+    post '/todos/new' do
+        title = params['title']
+        color = params['color']
+        description = params['description']
+        priority = params['priority']
+
+        db.execute("INSERT INTO todos (title, color, description, priority) VALUES(?,?,?,?)", [title, color, description, priority])
+        redirect("/todos")
+    end
+
 end
