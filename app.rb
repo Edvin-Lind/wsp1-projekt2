6ea2f1fee8e1f0be7c301686517a2079d0a03d22
Edvin-Lind <edvin.lind@elev.ga.ntig.se>
2024-11-25 14:41:46 +0100

    yers

--- app.rb
+++ app.rb
@@ -1,22 +1,46 @@
 class App < Sinatra::Base
 
+    def db
+        return @db if @db
+
+        @db = SQLite3::Database.new("db/todos.sqlite")
+        @db.results_as_hash = true
+
+        return @db
+    end
+
     get '/' do
+        redirect("views/index")
+    end
+
+    get '/index' do
+        @todos = db.execute('SELECT * FROM todos')
         erb(:"index")
     end
 
-    get '/todo' do
-        @fruits = db.execute('SELECT * FROM todos')
-        erb(:"todos/index")
+    get '/views/:id' do |id|
+        @todo = db.execute('SELECT * FROM todos WHERE id=?', id).first
+        erb(:"index")
     end
 
-    post '/todos/new' do
+    post '/views/index' do
         title = params['title']
         color = params['color']
         description = params['description']
         priority = params['priority']
 
         db.execute("INSERT INTO todos (title, color, description, priority) VALUES(?,?,?,?)", [title, color, description, priority])
-        redirect("/todos")
+        redirect("/index")
+    end
+
+    post '/views/index' do
+        title = params['title']
+        color = params['color']
+        description = params['description']
+        priority = params['priority']
+
+        db.execute("INSERT INTO todos (title, color, description, priority) VALUES(?,?,?,?)", [title, color, description, priority])
+        redirect("/index")
     end
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -3,9 +3,38 @@ require 'sqlite3'
 class Seeder
 
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS todos')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE todos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                color TEXT NOT NULL,
+                description TEXT,
+                priority INTEGER)')
+  end
+
+  def self.populate_tables
+    db.execute('INSERT INTO todos (title, color, description, priority) VALUES ("Äpple",   white, "En rund frukt som finns i många olika färger.", 3)')
+    db.execute('INSERT INTO todos (title, color, description, priority) VALUES ("Päron",    black, "En nästan rund, men lite avläng, frukt. Oftast mjukt fruktkött.", 2)')
+    db.execute('INSERT INTO todos (title, color, description, priority) VALUES ("Banan",  yellow, "En avlång gul frukt.", 1)')
+  end
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/todos.sqlite')
+    @db.results_as_hash = true
+    @db
+  end
 end
+
 
 Seeder.seed!
