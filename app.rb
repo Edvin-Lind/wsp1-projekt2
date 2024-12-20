95f0a528dd9a94b977373aeeb9a05ac03f46ee3c
Edvin-Lind <edvin.lind@elev.ga.ntig.se>
2024-12-20 01:38:43 +0100

    2024-12-20

--- app.rb
+++ app.rb
@@ -1,4 +1,7 @@
 class App < Sinatra::Base
+    configure do
+        enable :method_override
+      end
 
     def db
         return @db if @db
@@ -22,6 +25,29 @@ class App < Sinatra::Base
 
         db.execute("INSERT INTO todos (title, color, description, priority) VALUES(?,?,?,?)", [title, color, description, priority])
         redirect("/")
+    end
+
+    delete '/delete/:id' do
+        id = params['id']
+        db.execute('DELETE FROM todos WHERE id = ?', [id])
+        redirect('/')
+    end
+
+    get '/edit/:id' do
+        id = params['id']
+        @todo = db.execute('SELECT * FROM todos WHERE id = ?', [id]).first
+        erb(:edit)
+      end
+
+    post '/edit/:id' do
+        id = params['id']
+        title = params['title']
+        color = params['color']
+        description = params['description']
+        priority = params['priority']
+
+        db.execute('UPDATE todos SET title = ?, color = ?, description = ?, priority = ? WHERE id = ?', [title, color, description, priority, id])
+        redirect('/')
     end
 
 end
